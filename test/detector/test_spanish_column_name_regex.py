dd979083fecd1f0f02a7054bfa3da71fc16897da
Joaquín Leonel Robles <joaquinlrobles@gmail.com>
2025-01-16 13:54:53 +0000

    Remove __class__ references in tests

--- test/detector/test_spanish_column_name_regex.py
+++ test/detector/test_spanish_column_name_regex.py
@@ -5,52 +5,52 @@ from detectpii.pii_type import *
 
 def test_detect_person_pii_given_name_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="nombre")).__class__ == Person
+        column=Column(name="nombre")) == Person()
 
 def test_detect_email_pii_given_email_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="correo_electronico")).__class__ == Email
+        column=Column(name="correo_electronico")) == Email()
 
 def test_detect_birthdate_pii_given_birthdate_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="fecha_nacimiento")).__class__ == BirthDate
+        column=Column(name="fecha_nacimiento")) == BirthDate()
 
 def test_detect_gender_pii_given_gender_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="sexo")).__class__ == Gender
+        column=Column(name="sexo")) == Gender()
 
 def test_detect_nationality_pii_given_nationality_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="nacionalidad")).__class__ == Nationality
+        column=Column(name="nacionalidad")) == Nationality()
 
 def test_detect_address_pii_given_address_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="direccion")).__class__ == Address
+        column=Column(name="direccion")) == Address()
     
 def test_detect_zipcode_pii_given_zipcode_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="cp")).__class__ == ZipCode
+        column=Column(name="cp")) == ZipCode()
 
 def test_detect_username_pii_given_username_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="usuario")).__class__ == UserName
+        column=Column(name="usuario")) == UserName()
 
 def test_detect_password_pii_given_password_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="contrasenia")).__class__ == Password
+        column=Column(name="contrasenia")) == Password()
 
 def test_detect_ssn_pii_given_ssn_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="dni")).__class__ == SSN
+        column=Column(name="dni")) == SSN()
 
 def test_detect_pobox_pii_given_pobox_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="casilla_correo")).__class__ == PoBox
+        column=Column(name="casilla_correo")) == PoBox()
 
 def test_detect_creditcard_pii_given_creditcard_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="nro_tarjeta")).__class__ == CreditCard
+        column=Column(name="nro_tarjeta")) == CreditCard()
 
 def test_detect_phone_pii_given_phone_like_column_name():
     assert SpanishColumnNameRegexDetector().detect(
-        column=Column(name="telefono")).__class__ == Phone
+        column=Column(name="telefono")) == Phone()
